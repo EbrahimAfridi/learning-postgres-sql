a51e37eb1e0d97254d9ba258a4e3e73a1bfbbf04
ebrahim afridi <afridiebr@student.iul.ac.in>
2024-09-02 10:57:44 +0530

    add create table

--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,9 @@
-import { Client } from 'pg';
+import { Client } from "pg";
 
 export async function getClient() {
-    const client = new Client("postgres://wzsxsnxg:LHZ9Cv4QoZ1zctxapkOq2ch672-o9UQe@trumpet.db.elephantsql.com/wzsxsnxg");
-    await client.connect();
-    return client;
+  const client = new Client(
+    "postgresql://payment-db_owner:Zdvh5EMfRSu2@ep-crimson-night-a5l9y5i9.us-east-2.aws.neon.tech/payment-db?sslmode=require"
+  );
+  await client.connect();
+  return client;
 }
